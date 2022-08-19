518a79a4d1a3dac7a5fa064e157163e2b0537bdd
Choni Noa <choninoa@gmail.com>
2022-08-19 00:08:43 -0400

    Swagger and mongo config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true
+    }),
+  );   
+    
+  app.enableCors();
+  const version='v.220819.0003'
+  const options = new DocumentBuilder()
+    .setTitle('Musala-Test  - API Documentation')
+    .setDescription('The documentation')
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(
+    'api',
+    app,
+    document,
+  );
+
+ 
+  
+  
+  const port = configService.get<string>('PORT')||6000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
