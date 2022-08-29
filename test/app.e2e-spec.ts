7bb9675da3d8023e0828b542670ce202a2b9c204
Choni Noa <choninoa@gmail.com>
2022-08-29 09:15:11 -0400

    Fix e2e tests and add to github workflow action

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Gateway } from 'src/gateway/schemas/gateway.schema';
+import mongoose from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-
+  beforeAll(done => {
+    done()
+  });
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true
+      }),
+    );
     await app.init();
   });
 
@@ -21,7 +32,26 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+  describe('/gateway/create (POST) crete gateway ', () => {
+    it('Validate ipv4 creating gateway', async () => {
+      const mockGateway: Gateway = {
+        serialNumber: 'sn-234133',
+        name: 'Test Creating',
+        ipv4: '192.168.1.300'
+      }
+     return await request(app.getHttpServer())
+        .post('/gateway/create')
+        .send(mockGateway)
+        .expect(HttpStatus.BAD_REQUEST)
+    })
+
+  });
+
   afterAll(async () => {
-    await app.close()
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose.connection.close()
+
+    return await app.close()
+
   })
 });
